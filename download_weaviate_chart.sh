6937fc98923bbb014e99826e7319efc3e6753637
Etienne Dilocker <etienne@dilocker.de>
2020-02-10 14:42:19 +0100

    gh-1 fix chart issues

--- download_weaviate_chart.sh
+++ download_weaviate_chart.sh
@@ -4,7 +4,8 @@ set -e
 
 source versions.sh.inc
 
-rm -rf chart/charts && mkdir -p chart/charts
+rm -rf chart/weaviate-on-gke/charts && mkdir -p chart/weaviate-on-gke/charts
 
-curl -o chart/charts/weaviate.tgz "https://github.com/semi-technologies/weaviate-helm/releases/download/$WEAVIATE_HELM_CHART_VERSION/weaviate.tgz"
+set -x
+curl --fail -o chart/weaviate-on-gke/charts/weaviate.tgz -L "https://github.com/semi-technologies/weaviate-helm/releases/download/$WEAVIATE_HELM_CHART_VERSION/weaviate.tgz"
 # Download configuration values
