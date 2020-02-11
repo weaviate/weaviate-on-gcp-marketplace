8f9601350545996438c00c061f4ba3e2cada7258
Etienne Dilocker <etienne@dilocker.de>
2020-02-11 15:17:28 +0100

    gh-1 tag deployer with main version, build on pipeline

--- build_deployer.sh
+++ build_deployer.sh
@@ -4,5 +4,8 @@ source versions.sh.inc
 
 export REGISTRY=gcr.io/$GCP_PROJECT
 export APP_NAME=weaviate-on-gke
-docker build --tag "$TARGET_REPO_BASE/deployer" .
+docker build \
+  --tag "$TARGET_REPO_BASE/deployer" \
+  --tag "$TARGET_REPO_BASE/deployer:$TARGET_VERSION" \
+  .
 docker push "$TARGET_REPO_BASE/deployer"
