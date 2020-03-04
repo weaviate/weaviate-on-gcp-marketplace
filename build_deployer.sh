b45d76109382785b9778bc0ad061be603f7e0ad6
Etienne Dilocker <etienne@dilocker.de>
2020-03-04 14:51:59 +0100

    gh-1 make sure dummy values in value.yaml get overriden

--- build_deployer.sh
+++ build_deployer.sh
@@ -7,5 +7,12 @@ export APP_NAME=weaviate-on-gke
 docker build \
   --tag "$TARGET_REPO_BASE/deployer" \
   --tag "$TARGET_REPO_BASE/deployer:$TARGET_VERSION" \
+  --tag "$TARGET_REPO_BASE/deployer:$TARGET_IMAGE_VERSION" \
   .
-docker push "$TARGET_REPO_BASE/deployer"
+
+echo "Pushing latest"
+docker push "$TARGET_REPO_BASE/deployer:latest"
+echo "Pushing full target"
+docker push "$TARGET_REPO_BASE/deployer:$TARGET_VERSION"
+echo "Pushing short target"
+docker push "$TARGET_REPO_BASE/deployer:$TARGET_IMAGE_VERSION"
