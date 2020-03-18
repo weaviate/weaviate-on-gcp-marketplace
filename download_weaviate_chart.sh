d2122bed7bf86213b732e9f69622cf2f1654fff0
Etienne Dilocker <etienne@dilocker.de>
2020-03-18 15:59:37 +0100

    gh-1 include helm chart from forked repo

--- download_weaviate_chart.sh
+++ download_weaviate_chart.sh
@@ -7,5 +7,5 @@ source versions.sh.inc
 rm -rf chart/weaviate-enterprise/charts && mkdir -p chart/weaviate-enterprise/charts
 
 set -x
-curl --fail -o chart/weaviate-enterprise/charts/weaviate.tgz -L "https://github.com/semi-technologies/weaviate-helm/releases/download/$WEAVIATE_HELM_CHART_VERSION/weaviate.tgz"
+curl --fail -o chart/weaviate-enterprise/charts/weaviate.tgz -L "https://github.com/semi-technologies/weaviate-helm-gcp-marketplace/releases/download/$WEAVIATE_HELM_CHART_VERSION/weaviate.tgz"
 # Download configuration values
